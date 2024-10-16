58e1f073ae0918784dda72669b3ea7da26979e0d
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-10-16 19:50:09 +0200

    adding functionality with buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,19 @@ import NasaData from './components/NasaData';
 import { FaClock } from "react-icons/fa6";
 import { FaCalendarAlt } from "react-icons/fa";
 
-
-
 function Weather() {
   const [time, setTime] = useState(null);
   const [temperature, setTemperature] = useState(null);
   const [precipitationChance, setPrecipitationChance] = useState(null);
   const [precipitation, setPrecipitation] = useState(null);
-  const [rain, setRain] = useState(0)
+  const [rain, setRain] = useState(0);
   const [humidity, setHumidity] = useState(0);
   const [wind, setWind] = useState(0);
   const [loading, setLoading] = useState(true);
   const [whatTime, setWhatTime] = useState('');
-  const [date, setDate] = useState('')
+  const [date, setDate] = useState('');
+  const [pmTime, setPMTime] = useState('');
+  const [timeFormat, setTimeFormat] = useState(false);
 
   const stockSymbols = [
     "AAPL", "GOOGL", "TSLA", "MSFT", 
@@ -31,8 +31,7 @@ function Weather() {
   useEffect(() => {
     const getWeather = async () => {
       try {
-        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=50.85&longitude=4.85&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,precipitation,rain,snowfall,wind_speed_10m&timezone=Europe%2FBerlin'
-        );
+        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=50.85&longitude=4.85&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,precipitation,rain,snowfall,wind_speed_10m&timezone=Europe%2FBerlin');
         const data = await response.json();
 
         const currentDate = new Date();
@@ -40,27 +39,25 @@ function Weather() {
         const timezoneOffset = 2; // CEST is UTC+2
         currentDate.setHours(currentDate.getHours() + timezoneOffset);
         const currentTime = currentDate.toISOString().slice(0, 16);
-        
+
         setTime(currentTime);
-        
+
         const timeIndex = data.hourly.time.indexOf(currentTime);
-        
+
         if (timeIndex !== -1) {
           const precipitationChance = data.hourly.precipitation_probability ? data.hourly.precipitation_probability[timeIndex] : "no data available";
           const currentTemp = data.hourly.temperature_2m[timeIndex];
           const precipitation = data.hourly.precipitation[timeIndex];
           const rain = data.hourly.rain[timeIndex];
-          const currentHumidity = data.hourly.relative_humidity_2m[timeIndex]
-          const currentWind = data.hourly.wind_speed_10m[timeIndex]
+          const currentHumidity = data.hourly.relative_humidity_2m[timeIndex];
+          const currentWind = data.hourly.wind_speed_10m[timeIndex];
 
           setTemperature(currentTemp);
           setPrecipitationChance(precipitationChance);
           setPrecipitation(precipitation);
-          setRain(rain)
-          setHumidity(currentHumidity)
-          setWind(currentWind)
-          console.log(rain);
-          
+          setRain(rain);
+          setHumidity(currentHumidity);
+          setWind(currentWind);
         } else {
           console.error('Current time not found in the data');
         }
@@ -70,7 +67,7 @@ function Weather() {
         setLoading(false);
       }
     };
-  
+
     setInterval(() => {
       getWeather();
     }, 1800000);
@@ -78,14 +75,16 @@ function Weather() {
     getWeather();
   }, []);
 
-
   useEffect(() => {
     const updateTime = () => {
       const theTime = new Date();
-      const date = theTime.toLocaleDateString()
+      const date = theTime.toLocaleDateString();
       const localTime = theTime.toLocaleTimeString([], { hour12: false });
+      const pmTime = theTime.toLocaleTimeString([], { hour12: true });
+
       setWhatTime(localTime);
-      setDate(date)
+      setPMTime(pmTime);
+      setDate(date);
     };
 
     updateTime(); // Update immediately on mount
@@ -93,39 +92,41 @@ function Weather() {
 
     return () => clearInterval(intervalId); // Cleanup interval on unmount
   }, []);
-  
-
-
-    
 
   return (
     <div className='background w-dvw h-dvh'>
       <div id="topSection" className='w-full h-[40%] flex'>
         <div id="weatherDiv" className="relative topLeft w-[50%] h-full flex justify-end items-center pr-[6.5rem]">
-          <div id='dashboardDiv' className="absolute left-6 top-6 font-bold text-3xl text-white max-[950px]:top-2 max-[950px]:relative max-[950px]:mr-[3rem] flex flex-col w-[14rem] h-[4rem] bg-opacity-25 rounded-md bg-zinc-400">
-            <div className="flex w-full h-[2.2rem]  justify-between items-center px-2">
+          <div id='dashboardDiv' className="absolute left-6 top-6 font-bold text-3xl text-white max-[950px]:top-2 max-[950px]:relative max-[950px]:mr-[3rem] flex flex-col w-[15rem] h-[4rem] bg-opacity-25 rounded-md bg-zinc-400">
+            <div className="flex w-full h-[2.2rem] justify-between items-center gap-2 px-2">
               <h1 className='text-[1.3rem] font-bold'>Dashboard</h1>
               <div className="flex gap-1 w-[5rem] h-[1.6rem] rounded-md bg-zinc-100 justify-center items-center bg-opacity-30">
-
-                  <FaClock className='text-sm'/>
+                <FaClock className='text-sm'/>
                 <p className='text-[18px] font-medium'>
-                  {whatTime.slice(0, 2) + ' : ' + whatTime.slice(3, 5)}
+                  {timeFormat ? whatTime.slice(0, 2) + ' : ' + whatTime.slice(3, 5) : pmTime.slice(0, 1) + ' : ' + pmTime.slice(2, 4) }
                 </p>
+              </div>
+              <div className="flex w-[1.9rem] h-[1.6rem] rounded-md bg-green-400 justify-center items-center bg-opacity-50 hover:border-[1px] duration-200 ease-linear hover:border-white">
+                <button className='text-[10px] ' onClick={() => setTimeFormat(!timeFormat)}>
+                  {timeFormat ? '24 H' : pmTime.slice(-2)}
+                </button>
               </div>
             </div>
             <div className="flex w-full h-[1.8rem] bg-zinc-100 bg-opacity-35 justify-center items-center rounded-b-lg gap-7">
-              
-              <p className='text-[18px]  font-thin flex justify-center items-center gap-1'><FaCalendarAlt className='text-sm'/>
-              {date}</p>
+              <p className='text-[18px] font-thin flex justify-center items-center gap-1'>
+                <FaCalendarAlt className='text-sm'/> {date}
+              </p>
             </div>
-
           </div>
-          {loading ? 
-            <WeatherLoading/>
-            :
-            <WeatherData temperature={temperature} 
-            precipitation={precipitation} precipitationChance={precipitationChance}
-            time={time} rain={rain} humidity={humidity} wind={wind}
+          {loading ? <WeatherLoading/> :
+            <WeatherData
+              temperature={temperature} 
+              precipitation={precipitation} 
+              precipitationChance={precipitationChance}
+              time={time} 
+              rain={rain} 
+              humidity={humidity} 
+              wind={wind}
             />
           }
         </div>
@@ -136,11 +137,9 @@ function Weather() {
       </div>
 
       <div id="botSection" className="w-full h-[60%] flex">
-
-        <div className="botLeft flex flex-col w-[50%] h-full justify-center  items-center pl-[4rem] pb-[2rem]">
+        <div className="botLeft flex flex-col w-[50%] h-full justify-center items-center pl-[4rem] pb-[2rem]">
           <StockData symbols={stockSymbols} /> {/* Pass array of symbols here */}
         </div>
-
         <div className="botRight w-[50%] h-full flex justify-center items-center">
           <NewsData/>
         </div>
--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import { FaCloud } from "react-icons/fa";
 import { MdOutlineOnlinePrediction } from "react-icons/md";
+import { MdOutlineSwitchAccessShortcutAdd } from "react-icons/md";
 
 import { FaWind } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
@@ -10,13 +12,29 @@ import { FaDroplet } from "react-icons/fa6";
 
 
 export default function WeatherData({temperature, precipitation, precipitationChance, time, rain, humidity, wind}) {
+    const [unit, setUnit] = useState(false);
+    const [displayTemp, setDisplayTemp] = useState(temperature);  // local state to track the display temperature
+
+    const switchTempUnit = () => {
+        setUnit(prevUnit => !prevUnit);  
+
+        if (!unit) {
+            // Celsius to Fahrenheit
+            setDisplayTemp((temperature * 1.8) + 32);
+         
+        } else {
+            // Fahrenheit to Celsius
+            setDisplayTemp((displayTemp - 32) / 1.8);
+
+        }
+    };
     
 
     return (
     <div className="relative w-[18rem] h-[12rem] rounded-xl bg-slate-200 bg-opacity-45">
         <div className="flex items-center justify-between p-2 w-full h-[8rem]   rounded-t-xl ">
             <div className="left flex w-1/2 h-full  justify-center items-center">
-                <div className="w-[6.5rem] h-[6rem] overflow-hidden bg-zinc-100 bg-opacity-15 rounded-2xl flex justify-center items-center">
+                <div className="w-[6.5rem] h-[6rem]  overflow-hidden bg-zinc-100 bg-opacity-15 rounded-2xl flex justify-center items-center">
                     {
                     precipitation > 0 ?
                     <div className="rainy-cloud loader">
@@ -53,8 +71,13 @@ export default function WeatherData({temperature, precipitation, precipitationCh
                 </div> 
             </div>
 
-            <div className="right flex flex-col w-1/2 h-full  justify-center items-center">
-                <span className='font-bold text-[2.5rem]'>{temperature}°C</span>
+            <div className="right  pb-5 flex flex-col w-1/2 h-full   justify-center items-center">
+                <span className="flex">
+                    <button onClick={switchTempUnit} className='bg-zinc-200 bg-opacity-30 rounded-lg px-1 text-sm flex gap-1 justify-center items-center  text-white font-semibold hover:ring-[1px] duration-200 ease-linear hover:ring-white'><MdOutlineSwitchAccessShortcutAdd className='text-[12px]' />
+                    {unit ? 'Celsius' : 'Farenheit' }</button>
+                    
+                </span>
+                <span className='font-bold text-[2.5rem]'>{unit ? displayTemp.toFixed(1) + 'F' : displayTemp.toFixed(1) + '°C' }</span>
                 <span className='bg-zinc-200 pl-1 pr-2 rounded-lg bg-opacity-20 font-semibold pt-1 text-sm flex justify-center items-center gap-1 mb-[-10px]'><FaLocationDot className='mb-1 text-[13px]' />
                 50.85° N, 4.84° E</span>
             </div>
